c12a10f8cfc20b5163cbeb83b9f36e533476a0d5
haolin <haolin.h0@gmail.com>
2016-10-26 00:18:39 +0800

    version

--- diablo-client/src/main/java/me/hao0/diablo/client/DiabloAgent.java
+++ diablo-client/src/main/java/me/hao0/diablo/client/DiabloAgent.java
@@ -32,7 +32,7 @@ class DiabloAgent {
     /**
      * The client version
      */
-    private static final String CLIENT_VERSION = "1.1.0";
+    private static final String CLIENT_VERSION = "1.2.1";
 
     private AbstractDiabloClient client;
 
